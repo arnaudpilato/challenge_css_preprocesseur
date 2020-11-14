c2e0fd2b8c931e253f8ad8be1928599080807894
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-11-14 18:22:06 +0100

    create challenge

---
+++ public/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" rel="stylesheet" href="assets/css/style.css">
+    <title>Challenge CSS Preprocesseur</title>
+</head>
+<body>
+    <header class="nav-bar">
+        <h1>Find The Precious</h1>
+        <div class="link">
+            <a href="#">Fellows</a>
+            <a href="#">The Ring</a>
+            <a href="#">Reward</a>
+            <a href="#">Hunter</a>
+            <a href="#">Join</a>
+            <a href="#">Contact</a>
+        </div>
+    </header>
+    <section class="main">
+        <div class="ring-top">
+            <div class="ring"></div>
+            <h2>My Precious</h2>
+            <div class="ring"></div>
+        </div>
+        <div class="ring-middle">
+            <div class="ring"></div>
+            <h2>The One Ring</h2>
+        </div>
+        <div class="ring-bottom">
+            <div class="ring"></div>
+            <div class="ring"></div>
+            <div class="ring"></div>
+            <div class="ring"></div>
+            <div class="ring"></div>
+            <div class="ring"></div>
+        </div>
+        <div>
+            <h2>Please Find it !</h2>
+        </div>
+    </section>
+    <footer class="footer">
+        <div class="footer-link">
+            <a href="#">About Us</a>
+            <a href="#">Fellows</a>
+            <a href="#">Join Our Army</a>
+        </div>
+        <div class="footer-link">
+            <a href="#">FAQ</a>
+            <a href="#">Reward Conditions</a>
+            <a href="#">Legal Mentions</a>
+        </div>
+        <div class="footer-link">
+            <a href="#">sauron4ever.com</a>
+            <a href="#">Follow Him Also On Twitter</a>
+            <a href="#">FindThePrecious</a>
+        </div>
+    </footer>
+</body>
+</html>
